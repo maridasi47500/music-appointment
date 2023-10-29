7909fc5c3ece6f77942568f445a012d50d4ba375
anonymedelaregion <anonyme201504@gmail.com>
2023-10-28 23:45:09 -0300

    player

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,16 +9,37 @@ import { SongService } from './../shared/song.service';
 	      styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+	currentTimeContainer:any="00:00";
+	durationContainer:any="00:00";
+	playpause:any="";
+	slidervalue:any=0.0;
+	sliderMax:any=100.0;
 	  Bookings: any = [];
 	  mykeys: any = [];
 	  mysrc: string="";
 	  mySongs: any = {};
 	  audio:any;
+
 	  Songs: any = [];
 	    constructor(private aptService: AppointmentService, private songService: SongService) {}
 	      ngOnInit() {
-		          this.fetchBookings();
-			      let bookingRes = this.aptService.getBookingList();
+																									  this.audio = new Audio(); 
+																									  this.audio.addEventListener('timeupdate', () => {
+  this.currentTimeContainer = this.calculateTime(this.audio.currentTime);
+  this.durationContainer = this.calculateTime(this.audio.duration);
+  this.slidervalue = Math.floor(this.audio.currentTime);
+});
+if (this.audio.readyState > 0) {
+  this.displayDuration()
+  this.setSliderMax()
+} else {
+  this.audio.addEventListener('loadedmetadata', () => {
+    this.displayDuration()
+  this.setSliderMax() 
+  })
+}
+		          this.fetchBookings()
+			      let bookingRes = this.aptService.getBookingList()
 			          bookingRes.snapshotChanges().subscribe((res) => {
 					        this.Bookings = [];
 						      res.forEach((item) => {
@@ -76,26 +97,68 @@ export class HomePage implements OnInit {
 																								download(url: string){
 																									  window.open(url, "_blank");
 																								}
+
+																								calculateTime (secs:any)  {
+  const minutes = Math.floor(secs / 60);
+  const seconds = Math.floor(secs % 60);
+  const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`
+  return `${minutes}:${returnedSeconds}`
+}
+
+displayDuration () {
+  this.durationContainer = this.calculateTime(this.audio.duration)
+  this.slidervalue = Math.floor(this.audio.currentTime)
+}
 																								pausemusic(){
-																									  if(this.audio){
+																									  if(this.audio && !this.audio.paused){
 																										  this.audio.pause()
-																										  this.audio.currentTime=0
+																										  //this.audio.currentTime=0
+																									  this.playpause="play"
+																									  }else if(this.audio){
+																										  this.audio.play()
+																									  this.playpause="pause"
 																									  }
 																								}
+
+
 																								jouer(url: string){
-																									  if(this.audio){
+																									
+																									  if(this.audio && !this.audio.paused){
+																									  if(!this.audio.paused){
+
 																										  this.audio.pause()
-																										  this.audio.currentTime=0
 																									  }
-																									  this.audio = new Audio(); 
+																										  this.audio.currentTime=0
+
+																									  }
+
 																									  this.audio.src = url
-																									  this.audio.load()
+
+																									  //this.audio.load()
+																										  this.audio.currentTime=0
 																									  this.audio.play()
+																									  this.playpause="pause"
+
 
 																								}
 																								jouerchanson($ev:any){
 																									  $ev.target.play()
 																								}
+																							setSliderMax() {
+  this.sliderMax = Math.floor(this.audio.duration)
+}
 
+myInput($ev:any){
+	  this.currentTimeContainer = this.calculateTime($ev.target.value)
+	  
+
+}
+myClick($ev:any){
+
+}
+myChange($ev:any){
+	this.audio.currentTime=$ev.target.value
+
+}
 
 }
