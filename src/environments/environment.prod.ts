9a0c21eb9d90544b0efac289dcefd16587cddeec
pirate du bato <anonyme201504@gmail.com>
2024-08-29 23:54:59 +0200

    hey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+  apiKey: "AIzaSyDNvae1Ua8owJ6hejNCBr6A9bGwltmpvi0",
+  authDomain: "ionic-firebase-1c6ac.firebaseapp.com",
+  databaseURL: "https://ionic-firebase-1c6ac-default-rtdb.firebaseio.com",
+  projectId: "ionic-firebase-1c6ac",
+  storageBucket: "ionic-firebase-1c6ac.appspot.com",
+  messagingSenderId: "883659286482",
+  appId: "1:883659286482:web:d9e4f53754985605ed0a0b"
+}
 };
